c8e060dcf72cd1ee4cdf343ad535d3a5ce224369
Marilia <devmariliasales@gmail.com>
2024-09-18 14:43:00 -0300

    Main

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,6 +1,6 @@
 import '../Styles/Main.css';
 import img from '../assets/Image.png'
-
+import foto from '../assets/Avatar.png'
 export default function Main() {
     return (
         <div className="main">
@@ -15,6 +15,63 @@ export default function Main() {
                     </div>
                 </div>
             </main>
+
+                <section>
+                    <div className='vagas'>
+                        <div className='content-vaga'>
+                            <h4>Design</h4>
+
+                            <div className='info'>
+                            <h4>Product Designer</h4>
+                                <span>Full-time</span>
+                                <span>Faixa salarial:R$7.000</span>
+                            </div>
+
+                            <div className='info'>
+                            <h4>Product Designer</h4>
+                                <span>Full-time</span>
+                                <span>Faixa salarial:R$7.000</span>
+                            </div>
+
+                           
+                        </div>
+
+                        <div className='content-vaga'>
+                            <h4>Desenvolvimento de Software</h4>
+
+                            <div className='info'>
+                            <h4>Desenvolvedor(a) iOS</h4>
+                                <span>Full-time</span>
+                                <span>Faixa salarial:R$8.000</span>
+                            </div>
+
+                            <div className='info'>
+                            <h4>Desenvolvedor(a) Front-end</h4>
+                                <span>Full-time</span>
+                                <span>Faixa salarial:R$7.000</span>
+                            </div>
+
+                            <div className='info'>
+                            <h4>Desenvolvedor(a) Back-end</h4>
+                                <span>Full-time</span>
+                                <span>Faixa salarial:R$8.000</span>
+                            </div>
+                        </div>
+                    </div>
+                </section> 
+
+                <section>
+                    <div className='content-depoimento'>
+                        <span id='title'>Veja o que nossos colaboradores falam sobre nós</span>
+                        <p>OptimusTech se importa com a sáude dos seus colaboradores e <br/> sempre procura nos da todo tipo de auxílio possível.</p>
+
+                        <div className='info-depoimento'>
+                            <img src={foto}></img>
+                            <span id='name'>Júlia Castro</span>
+                            <span>Desenvolvedora Web</span>
+                        </div>
+                    </div>
+                </section>
         </div>
     )
 
